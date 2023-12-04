9afd2d1dd591ef4641ca0b74af8af0dc444b5ac3
pinea <121351623@umail.ucc.ie>
2023-12-04 20:53:53 +0000

    Added itUserPay() and editUserHours() to database

--- database.py
+++ database.py
@@ -16,8 +16,24 @@ c.execute("""
 )""")
 
 
-c.execute("INSERT INTO employees VALUES (?,?,?,?)", (1234, "Sam", 14, 0))
+
 conn.commit()
+def insertUser(employeeID,name,pay,hoursWorked=0):
+    c.execute("INSERT INTO employees VALUES (?,?,?,?)", (employeeID, name, pay, hoursWorked))
+
+
+
+def editUserPay(employeeID,newPay):
+    c.execute("UPDATE employees  SET Pay = ? WHERE UserID =?",(newPay,employeeID))
+    
+
+def editUserHours(employeeID,newHours):
+    c.execute("UPDATE employees  SET hoursWorked = ? WHERE UserID =?",(newHours,employeeID))
+    
+c.execute("INSERT INTO employees VALUES (?,?,?,?)",(1234,"Sam",14,0))
+editUserPay(1234,15)
+editUserHours(1234,2.5)
+
 
 c.execute("SELECT * FROM employees")
 print(c.fetchall())
