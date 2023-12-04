763c3a741cbb30ec92a01d59fff870b8756d9d28
pinea <121351623@umail.ucc.ie>
2023-12-04 20:35:30 +0000

    Changed database table to include necessary info

--- database.py
+++ database.py
@@ -5,15 +5,18 @@ conn = sqlite3.connect('employee.db')
 
 c = conn.cursor()
 c.execute("""DROP TABLE IF EXISTS employees """)
-c.execute("""CREATE TABLE employees (
-    first text,
-    last text,
-    pay integer
+c.execute("""          
+    CREATE TABLE employees (
+    UserID INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    Pay REAL NOT NULL,
+    HoursWorked REAL 
+
 
 )""")
 
 
-c.execute("INSERT INTO employees VALUES (?,?,?)", ("Sam", "ODonoghue", 14))
+c.execute("INSERT INTO employees VALUES (?,?,?,?)", (1234, "Sam", 14, 0))
 conn.commit()
 
 c.execute("SELECT * FROM employees")
